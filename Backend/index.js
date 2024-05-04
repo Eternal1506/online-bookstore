d55a958db5f940c26ca987b2b36cdd35aaf1707d
Pratham Chauhan <pratham.chauhan152005@gmail.com>
2024-05-03 22:19:06 -0400

    Add frontend components

--- Backend/index.js
+++ Backend/index.js
@@ -9,6 +9,15 @@ const app = express();
 //Middleware to parse the request body as JSON
 app.use(express.json());
 
+// Middleware for CORS
+app.use(
+    cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get('/', (req, res) => {
     console.log(req);
     return res.status(234).send('Welcome to the Bookstore' );
