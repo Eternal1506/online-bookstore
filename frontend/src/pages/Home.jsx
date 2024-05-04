e6f473181e491c9b82b9711a1bf94d3a1bf25b3b
Pratham Chauhan <pratham.chauhan152005@gmail.com>
2024-05-04 17:15:04 -0400

    Basic UI complete for the store

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -33,23 +33,23 @@ const Home = () => {
       {loading ? (
         <Spinner />
       ) : (
-        <table className="w-full border-seprate border-spacing-2">
+        <table className="w-full border-separate border-spacing-2">
           <thead>
             <tr>
-              <th className="broder broder-slate-600 rounded-md">No</th>
-              <th className="broder broder-slate-600 rounded-md">Title</th>
-              <th className="broder broder-slate-600 rounded-md max-md:hidden">
+              <th className="border border-slate-600 rounded-md">No</th>
+              <th className="border border-slate-600 rounded-md">Title</th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
                 Author
               </th>
-              <th className="broder broder-slate-600 rounded-md max-md:hidden">
+              <th className="border border-slate-600 rounded-md max-md:hidden">
                 Publish Year
               </th>
-              <th className="broder broder-slate-600 rounded-md">Operations</th>
+              <th className="border border-slate-600 rounded-md">Operations</th>
             </tr>
           </thead>
           <tbody>
             {books.map((book, index) => (
-              <tr key={book.id} className="h-8">
+              <tr key={book._id} className="h-8">
                 <td className="border border-slate-700 rounded-md text-center">
                   {index + 1}
                 </td>
@@ -60,7 +60,7 @@ const Home = () => {
                   {book.author}
                 </td>
                 <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {book.publish_year}
+                  {book.publishYear}
                 </td>
                 <td className="border border-slate-700 rounded-md text-center">
                   <div className="flex justify-center gap-x-4">
@@ -68,10 +68,10 @@ const Home = () => {
                       <BsInfoCircle className="text-2xl text-green-800" />
                     </Link>
                     <Link to={`/books/edit/${book._id}`}>
-                      <AiOutlineEdit className="text-2xl text-yellow-800" />
+                      <AiOutlineEdit className="text-2xl text-yellow-600" />
                     </Link>
                     <Link to={`/books/delete/${book._id}`}>
-                      <MdOutlineDelete className="text-2xl text-red-800" />
+                      <MdOutlineDelete className="text-2xl text-red-600" />
                     </Link>
                   </div>
                 </td>
